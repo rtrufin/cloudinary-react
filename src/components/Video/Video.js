e91a64ad8e8a5bb3abce4439d8934ab3627fabef
ENDAVA\rtrufin <razvan.trufin@endava.com>
2018-05-16 15:59:55 +0300

    Added inner ref

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -18,7 +18,7 @@ class Video extends CloudinaryComponent {
   }
 
   render() {
-    let {publicId, poster, sourceTypes, fallback, innerRef, sourceTransformation: sourceTransformations, ...options} = Object.assign({},
+    let {publicId, poster, sourceTypes, fallback, handleRef, sourceTransformation: sourceTransformations, ...options} = Object.assign({},
       this.context,
       this.props);
     sourceTransformations = sourceTransformations || {};
@@ -53,7 +53,7 @@ class Video extends CloudinaryComponent {
     }
 
     return (
-      <video innerRef={innerRef} {...tagAttributes}>
+      <video ref={handleRef} {...tagAttributes}>
         {sources}
         {fallback}
         {this.props.children}
